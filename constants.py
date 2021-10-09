f2f8b2b16d2727c2c5899e44f00011595b00130a
Allen Wang <abmathcounts@gmail.com>
2021-10-09 18:53:00 -0400

    updated gitignore and added sample constants

--- constants.py
+++ constants.py
@@ -1,9 +1,24 @@
-SUBJECT="""Subject line goes here for {name}"""
+SUBJECT="""ABMC 2021 Annual Competition Team Score Report"""
 
-BODY="""Dear {name},
+BODY="""Greetings team {team},
 
-Thank you for reaching out to massmail! On the day {date}, you left us a note on 
-our page that said {quote}. Thank you for your message and we wish you a good day.
+Thanks for participating in the ABMC annual competition! The solutions and results are now available on our website. If you have questions about your score, feel free to email us with questions.
 
-Best,
-massmail"""
+Below are the details for your team score:
+
+Name/Organization: {team}
+Team Member 1 email: {email_1}
+Team Member 2 email: {email_2}
+Team Member 3 email: {email_3}
+Team Member 4 email: {email_4}
+
+Individuals Aggregate Scaled: {saggregate}
+
+Team Round Score: {team_score}
+
+Team Total Score: {overall}
+
+For more information about our grading system, please see the Rules tab of our website.
+
+Best regards,
+The ABMC Team"""
